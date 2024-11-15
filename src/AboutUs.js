bf8ff70a3db26641a66987c1fe31255799c11caa
unknown <siwarba02@gmail.com>
2024-11-15 16:13:50 +0100

    test

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -9,35 +9,45 @@ function AboutUs() {
             <p>
                 We are a dynamic platform dedicated to bridging the gap between startups and talented professionals. Our mission is to empower innovation and creativity by connecting startups with the right resources, tools, and people to thrive in today’s competitive landscape.
             </p>
+            <br>
+            </br>
             <h2>Our Mission</h2>
             <p>
                 To create an ecosystem where startups can grow and succeed by finding the talent they need and fostering meaningful collaborations. We aim to simplify the recruitment process, making it easier for companies to focus on what truly matters: their vision.
             </p>
-            <h2>What We Do</h2>
+            <br>
+            </br>
+            <h2>What We Do ?</h2>
             <ul>
                 <li>A curated list of job opportunities in the startup world.</li>
                 <li>Tools for startups to showcase their culture and values.</li>
                 <li>A user-friendly interface for professionals to explore exciting roles that align with their skills and passions.</li>
             </ul>
+            <br>
+            </br>
             <h2>Our Values</h2>
             <ul>
                 <li><strong>Innovation:</strong> We celebrate creativity and fresh ideas.</li>
                 <li><strong>Collaboration:</strong> We believe in the power of partnerships.</li>
                 <li><strong>Empowerment:</strong> We support startups and professionals in achieving their goals.</li>
             </ul>
+            <br>
+            </br>
             <h2>Why Choose Us?</h2>
             <p>
                 Whether you're a startup seeking talent or a professional looking for your next adventure, we are here to make the process seamless, efficient, and rewarding. Together, let’s build the future of innovation.
             </p>
+            <br>
+            </br>
             <h2>The Minds Behind the Platform</h2>
             <div className="founders">
                 <div>
                     <img src={photorihem} alt="Rihem" />
-                    <p>Rihem - Co-Founder</p>
+                    <p>Rihem Farhat - Co-Founder</p>
                 </div>
                 <div>
                     <img src={photosiwar} alt="Siwar" />
-                    <p>Siwar - Co-Founder</p>
+                    <p>Siwar Ben Ammar- Co-Founder</p>
                 </div>
             </div>
         </div>
